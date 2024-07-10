8b6024e6548ba9a6bceb2ae4f2e55bed25be8943
robedem <122899474+robedem@users.noreply.github.com>
2024-07-10 15:48:26 +0100

    done

--- checkout.js
+++ checkout.js
@@ -18,3 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
         input.value = input.placeholder;
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.navmenu ul');
+    const cancel = document.querySelector('.cancel');
+
+    hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+        cancel.classList.toggle('active'); // Toggle cancel icon
+    });
+
+    cancel.addEventListener('click', function () {
+        navMenu.classList.remove('active');
+        cancel.classList.remove('active');
+    });
+});
